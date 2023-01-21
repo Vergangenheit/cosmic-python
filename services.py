7970ad0d316000d8e6850f7d68ddddc357d5af2b
Loloheia <lorenzo.ostano@vx.se>
2023-01-21 12:29:48 +0100

    dang if you didn't

--- services.py
+++ services.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import model
 from model import OrderLine
 from repository import AbstractRepository
+from sqlalchemy.orm import Session
 
 
 class InvalidSku(Exception):
@@ -13,7 +14,7 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+def allocate(line: OrderLine, repo: AbstractRepository, session: Session) -> str:
     batches = repo.list()
     if not is_valid_sku(line.sku, batches):
         raise InvalidSku(f"Invalid sku {line.sku}")
